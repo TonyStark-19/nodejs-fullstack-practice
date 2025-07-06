8ff05a10ef130fcb71fed891c33c9c54aaebea1c
Aditya chandel <adityachandel371@gmail.com>
2025-07-06 13:21:30 +0530

    Made UI + connected it to backend

--- quote-app/frontend/src/App.jsx
+++ quote-app/frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import use effect and use state
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// app function
+export default function App() {
+  // State to hold the current quote
+  const [quote, setQuote] = useState("Loading...");
+
+  // Function to fetch a random quote from the backend
+  const getQuote = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/quote");
+      const data = await res.json();
+      setQuote(data.quote); // Update the quote in UI
+    } catch (error) {
+      setQuote("Failed to fetch quote.");
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  // Fetch a quote on first render
+  useEffect(() => {
+    getQuote();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen flex justify-center items-center bg-zinc-900">
+      <div className="rounded-xl py-6 px-10 flex justify-center items-center flex-col w-[800px]
+      text-white bg-zinc-800">
+        <h1 className="text-5xl font-bold pb-7">Quote Generator App!</h1>
+
+        <p className="text-3xl pb-7 text-center leading-10">{quote}</p>
+
+        <button
+          onClick={getQuote}
+          className="text-xl py-2 px-3 rounded-2xl cursor-pointer
+          border-2 outline-none border-white">Get New Quote</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
