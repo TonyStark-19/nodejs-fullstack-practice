8d388d194f8ce1644acad962e7b48cbd0f758619
Aditya chandel <adityachandel371@gmail.com>
2025-08-01 12:37:12 +0530

    Added improvements

--- quote-app/frontend/src/App.jsx
+++ quote-app/frontend/src/App.jsx
@@ -27,16 +27,19 @@ export default function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-indigo-300">
       <div className="rounded-xl flex justify-center items-center flex-col text-black bg-slate-100 font-sans
-      min-a:w-[800px] min-a:py-8 min-a:px-10 max-a:w-[600px] max-a:py-6 max-a:px-6 max-b:w-[400px] max-c:w-[300px]">
-        <h1 className="font-semibold min-b:pb-7 max-b:pb-5
-        min-b:text-4xl max-b:text-3xl">Quote of the day</h1>
+      min-a:w-[700px] min-a:py-8 min-a:px-10 max-a:py-5 max-a:px-6 max-a:w-[90%]">
+        <h1 className="font-semibold min-b:pb-5 max-b:pb-3 min-b:text-[32px] max-b:text-[26px] relative after:content-[''] 
+        after:block after:w-30 after:h-1 after:bg-blue-500 after:mt-2 after:mb-3 after:rounded-full after:mx-auto">
+          Quote of the day
+        </h1>
 
-        <p className="text-center min-a:pb-10 max-a:pb-8
-        min-a:text-3xl min-a:leading-10 max-a:text-2xl max-a:leading-8">{quote}</p>
+        <p className="text-center min-a:pb-10 max-a:pb-8 min-a:text-[25px] min-a:leading-10 max-a:text-[22px] max-a:leading-8">
+          "{quote}"
+        </p>
 
         <button
           onClick={getQuote}
-          className="text-lg rounded-4xl cursor-pointer bg-blue-500 text-white
+          className="text-lg rounded-4xl cursor-pointer bg-blue-500 hover:bg-blue-700 transition text-white
           min-a:py-2 min-a:px-7 max-a:py-1.5 max-a:px-5">New Quote</button>
       </div>
     </div>
